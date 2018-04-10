c3a35ecc5bbe1897fcb9f21d7b9deab023ad1694
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-04-10 10:30:53 -0700

    wrote test to see if the file paths match

--- test/Logger.test.js
+++ test/Logger.test.js
@@ -1,16 +1,25 @@
 const assert = require('assert');
+const fs = require('fs');
 const Logger = require('../lib/Logger');
 
-describe('Logger', () => {
+describe('Logger', () => {    
+
+    const logger = new Logger('./log.txt');
 
     it('takes a message and returns a formatted log', () => {
         const message = 'Hello World!';
-        const logger = new Logger();
         const log = logger.log(message);
 
         const date = new Date();
-        const expected = `${date} ** ${message} sent by the client`;
+        const expected = `${date} ** ${message} sent by the client \n`;
         assert.equal(expected, log);
+    });
+
+    it('check the file path', () => {
+        const loggedFile = fs.readFileSync(`./lib/${logger.logfile}`);
+
+        const expected = fs.readFileSync('./lib/log.txt');
+        assert.deepEqual(expected, loggedFile);
     });
 
 });
